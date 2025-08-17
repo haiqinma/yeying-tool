593aca434e8fe4f542f5390ded4d49afd3ac85a2
yeying <yeying.community@gmail.com>
2025-08-17 17:23:54 +0800

    remove --no-discovery.

--- blockchain/ethereum/network/beacon/beacon-service.sh
+++ blockchain/ethereum/network/beacon/beacon-service.sh
@@ -248,7 +248,6 @@ start_beacon() {
         --p2p-tcp-port 13000 \
         --p2p-udp-port 12000 \
         --p2p-host-ip ${NAT_IP} \
-        --no-discovery \
         $BEACON_BOOTNODE \
         --execution-endpoint http://localhost:8551 \
         --accept-terms-of-use \
