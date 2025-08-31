533a1ae6bfb35e32eb74037ba848e9af52485599
youxuehu <youxuehu@hotmail.com>
2025-09-01 00:14:41 +0800

    制作 python flask 工程脚手架。基于 openapi

--- script/python.sh
+++ script/python.sh
@@ -1,19 +1,19 @@
-echo "Java 脚手架"
-echo "根据 yeying-idl 初始化 springboot 工程"
+echo "Python 脚手架"
+echo "根据 yeying-idl 初始化 Flask 工程"
 set -x
 
 output_path=$1
 if [ -z "$output_path" ]; then
-    echo "生成错误，spring-boot工程目录不能为空"
+    echo "生成错误，Flask工程目录不能为空"
     exit 1
 else
-    echo "您的spring-boot工程目录为: $output_path"
+    echo "您的Flask工程目录为: $output_path"
 fi
 if [ -d "$output_path" ]; then
-    echo "生成错误，$output_path 该目录已存在，请重新指定spring-boot工程目录"
+    echo "生成错误，$output_path 该目录已存在，请重新指定Flask工程目录"
     exit 1
 else
-    echo "开始生成spring-boot工程"
+    echo "开始生成Flask工程"
     mkdir -p "$output_path"
 fi
 
@@ -36,24 +36,19 @@ swagger2openapi apidocs.swagger.json -o apidocs.openapi.json
 rm -f apidocs.swagger.json
 mv apidocs.openapi.json "$output_path"/apidocs.openapi.json
 
-# 生成 springboot 脚手架
+# 生成 Flask 脚手架
 npm install -g @openapitools/openapi-generator-cli
 openapi-generator-cli version
-
+dir_name=$(basename "$output_path")
 export OPENAPI_GENERATOR_CLI_MIRROR=https://maven.aliyun.com/repository/public
 openapi-generator-cli generate \
   -i "$output_path"/apidocs.openapi.json \
-  -g spring \
+  -g python-flask \
   -o "$output_path" \
-  --additional-properties=generatorVersion=7.14.0,repoUrl=https://maven.aliyun.com/repository/public,useSpringBoot3=True,java23=True,dateLibrary=java23,openApiNullable=False,library=spring-boot,jakarta=True,useJakartaEe=True,useTags=True,invokerPackage=com.yeying,apiPackage=com.yeying.api,modelPackage=com.yeying.model,configPackage=com.yeying.config
-
-# 修改 java 版本到23
-sed -i '' 's|<java.version>17</java.version>|<java.version>23</java.version>|g' "$output_path"/pom.xml
-# 修改 springboot 版本 3.5.0
-sed -i '' '/<parent>/,/<\/parent>/ s|<version>3.1.3</version>|<version>3.5.0</version>|' "$output_path"/pom.xml
+  --additional-properties=generatorVersion=7.14.0,projectName="$dir_name",packageVersion=0.1.0,packageName="$dir_name",supportPython2=false,serverPort=5000,apiPackage=apis,modelPackage=models,hideGenerationTimestamp=true,useConnexionMiddleware=true
 
 tar -zcf "$output_path".tar.gz "$output_path"
 #rm -rf "$output_path"
 echo "$output_path".tar.gz
-echo "生成的spring-boot工程路径：$output_path；请解压后使用"
+echo "生成的Flask工程路径：$output_path；请解压后使用"
 set +x
